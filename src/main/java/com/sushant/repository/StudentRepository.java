230d96a193c1c48eaefff5c51450e1ffe056daa9
Sushant750 <sonukumaroffc@gmail.com>
2022-05-15 18:57:36 +0530

    LinkforextraQueryAdded

--- src/main/java/com/sushant/repository/StudentRepository.java
+++ src/main/java/com/sushant/repository/StudentRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
-    @Query
     public List<Student> findByFirstName(String firstName);
     public List<Student> findByFirstNameContaining(String firstName);
     public List<Student> findByGuardianName(String guardianName);
